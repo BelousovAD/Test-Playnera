7280a163b323172221f195820098500d73e1ad89
Aleksey Belousov <ya.belousow-lesha2015@yandex.ru>
2026-03-24 17:22:47 +0800

    Добавлена анимация применения румян

--- Assets/Scripts/Animation/BlushAnimation.cs
+++ Assets/Scripts/Animation/BlushAnimation.cs
@@ -3,17 +3,22 @@ using DG.Tweening;
 using Makeup;
 using Reflex.Attributes;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Animation
 {
     internal class BlushAnimation : MakeupAnimation
     {
-        [SerializeField] private MakeupType _type;
+        private const MakeupType Type = MakeupType.Blush;
+        
         [SerializeField] private RectTransform _hand;
         [SerializeField] private RectTransform _tool;
+        [SerializeField] private Image _toolColor;
         [SerializeField] private Transform _target;
         [SerializeField] private Transform _leftTarget;
         [SerializeField] private Transform _rightTarget;
+        [SerializeField] private Image _selectedMakeup;
+        [SerializeField] private Image _currentMakeup;
         [SerializeField] private List<Transform> _colors = new ();
 
         private Makeup.Makeup _makeup;
@@ -26,7 +31,7 @@ namespace Animation
         {
             foreach (Makeup.Makeup makeup in makeups)
             {
-                if (makeup.Type == _type)
+                if (makeup.Type == Type)
                 {
                     _makeup = makeup;
                     break;
@@ -64,10 +69,13 @@ namespace Animation
                 .Append(_hand.DOMove(_rightTarget.position, 1f))
                 .Append(_hand.DOMove(_leftTarget.position, 1f))
                 .Append(_hand.DOMove(_rightTarget.position, 1f))
+                .AppendCallback(() => _makeup.Apply())
                 .Append(_hand.DOMove(_toolParent.position, 1f))
                 .AppendCallback(DropTool)
                 .Append(_hand.DOMove(_handDefaultPosition, 1f))
-                .AppendCallback(() => CanvasGroup.interactable = true);
+                .AppendCallback(() => CanvasGroup.interactable = true)
+                .Insert(2f, _selectedMakeup.DOFade(1f, 3f))
+                .Insert(2f, _currentMakeup.DOFade(0f, 3f));
         }
 
         private void DropTool()
@@ -88,6 +96,7 @@ namespace Animation
                 .Append(_hand.DOMove(_tool.position, 1f))
                 .AppendCallback(TakeTool)
                 .Append(_hand.DOMove(_colors[_makeup.Selection].position, 1f))
+                .Append(_toolColor.DOFade(1f, 1f))
                 .Append(_hand.DOMove((_target.position - _colors[_makeup.Selection].position) / 2, 1f).SetRelative())
                 .AppendCallback(() => CanvasGroup.interactable = true);
         }
