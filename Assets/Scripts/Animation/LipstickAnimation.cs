338fd2e24ed80034637218f71e3a64645ec73b52
Aleksey Belousov <ya.belousow-lesha2015@yandex.ru>
2026-03-24 17:47:17 +0800

    Создана анимация покраски губ

---
+++ Assets/Scripts/Animation/LipstickAnimation.cs
@@ -0,0 +1,106 @@
+using System.Collections.Generic;
+using DG.Tweening;
+using Makeup;
+using Reflex.Attributes;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Animation
+{
+    internal class LipstickAnimation : MakeupAnimation
+    {
+        private const MakeupType Type = MakeupType.Lipstick;
+        
+        [SerializeField] private RectTransform _hand;
+        [SerializeField] private Transform _target;
+        [SerializeField] private Transform _leftTarget;
+        [SerializeField] private Transform _rightTarget;
+        [SerializeField] private Image _selectedMakeup;
+        [SerializeField] private Image _currentMakeup;
+        [SerializeField] private List<Transform> _colors = new ();
+
+        private Makeup.Makeup _makeup;
+        private Transform _toolParent;
+        private RectTransform _tool;
+        private Vector3 _handDefaultPosition;
+        private Vector3 _toolDefaultPosition;
+
+        [Inject]
+        private void Initialize(IEnumerable<Makeup.Makeup> makeups)
+        {
+            foreach (Makeup.Makeup makeup in makeups)
+            {
+                if (makeup.Type == Type)
+                {
+                    _makeup = makeup;
+                    break;
+                }
+            }
+        }
+
+        private void Awake() =>
+            _handDefaultPosition = _hand.position;
+
+        private void OnEnable()
+        {
+            _makeup.Selected += Take;
+            Take();
+        }
+
+        private void OnDisable() =>
+            _makeup.Selected -= Take;
+
+        public override void ApplyMakeup()
+        {
+            if (_makeup.Selection == Makeup.Makeup.MinIndex)
+            {
+                return;
+            }
+
+            DOTween.Sequence()
+                .AppendCallback(() => CanvasGroup.interactable = false)
+                .Append(_hand.DOMove(_target.position, 1f))
+                .Append(_hand.DOMove(_leftTarget.position, 1f))
+                .Append(_hand.DOMove(_rightTarget.position, 1f))
+                .Append(_hand.DOMove(_leftTarget.position, 1f))
+                .Append(_hand.DOMove(_rightTarget.position, 1f))
+                .AppendCallback(() => _makeup.Apply())
+                .Append(_hand.DOMove(_toolParent.position, 1f))
+                .AppendCallback(DropTool)
+                .Append(_hand.DOMove(_handDefaultPosition, 1f))
+                .AppendCallback(() => CanvasGroup.interactable = true)
+                .Insert(2f, _selectedMakeup.DOFade(1f, 3f))
+                .Insert(2f, _currentMakeup.DOFade(0f, 3f));
+        }
+
+        private void DropTool()
+        {
+            _tool.SetParent(_toolParent);
+            _tool.anchoredPosition = _toolDefaultPosition;
+        }
+
+        private void Take()
+        {
+            if (_makeup.Selection == Makeup.Makeup.MinIndex)
+            {
+                return;
+            }
+
+            _toolParent = _colors[_makeup.Selection];
+            _tool = _toolParent.GetChild(0) as RectTransform;
+
+            DOTween.Sequence()
+                .AppendCallback(() => CanvasGroup.interactable = false)
+                .Append(_hand.DOMove(_tool!.position, 1f))
+                .AppendCallback(TakeTool)
+                .Append(_hand.DOMove((_target.position - _toolParent.position) / 2, 1f).SetRelative())
+                .AppendCallback(() => CanvasGroup.interactable = true);
+        }
+
+        private void TakeTool()
+        {
+            _tool.SetParent(_hand);
+            _tool.SetAsFirstSibling();
+        }
+    }
+}
